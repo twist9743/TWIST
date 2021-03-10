b25eaee5338d0dac8570b76d6ebe29e46919829e
Ivan <twist9743@mail.ru>
2021-03-10 21:51:45 +0300

    not full lab 1

---
+++ app/src/main/java/com/example/lunin_ia_191_352/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.lunin_ia_191_352.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
