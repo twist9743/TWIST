e58fc92cee325d13ce00935b73516b786903e25d
Ivan <twist9743@mail.ru>
2021-04-07 19:16:17 +0300

    lab4

--- app/src/main/java/com/example/lunin_ia_191_352/ui/Lab3/Lab3.kt
+++ app/src/main/java/com/example/lunin_ia_191_352/ui/Lab3/Lab3.kt
@@ -1,24 +1,18 @@
 package com.example.lunin_ia_191_352.ui.lab3
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.lunin_ia_191_352.R
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.*
-import org.jsoup.Jsoup
-import org.jsoup.Jsoup.*
-import org.jsoup.nodes.Document
-import org.jsoup.nodes.Element
-import org.jsoup.select.Elements
-
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.auth.VKAccessToken
+import com.vk.api.sdk.auth.VKAuthCallback
+import com.vk.api.sdk.auth.VKScope
 
 
 class Lab3Fragment : Fragment() {
@@ -26,40 +20,39 @@ class Lab3Fragment : Fragment() {
     private lateinit var lab3ViewModel: Lab3ViewModel
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         lab3ViewModel =
-                ViewModelProvider(this).get(Lab3ViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_lab3, container, false)
-        val btn = root.findViewById<Button>(R.id.button)
-        var text_html = root.findViewById<TextView>(R.id.editTextTextMultiLine)
-        var curs_backsa = root.findViewById<TextView>(R.id.textView2)
-
-        btn.setOnClickListener{
-            GlobalScope.async {
-                val doc: Document = Jsoup.connect("https://www.cbr.ru/").get()
-                val parse_test = doc.text()
-                val mainHeaderElements: Elements = doc.select("#content > div > div > div > div.home-main > div.home-main_aside > div > div.main-indicator_rates > div > div:nth-child(2) > div:nth-child(2)")
-                val test : String = mainHeaderElements.toString()
-                val oldValue = "<div class=\"col-md-2 col-xs-9 _right mono-num\">"
-                val newValue = ""
-                val test2 = test.replace(oldValue,newValue,ignoreCase=true)
-                val oldValue2 = "</div>"
-                val final = test2.replace(oldValue2,newValue,ignoreCase=true)
-                while (curs_backsa.toString().isNotEmpty() and text_html.toString().isNotEmpty()){
-                    curs_backsa.text = final
-                    text_html.text = parse_test
-                }
-
-            }
+            ViewModelProvider(this).get(Lab3ViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_lab4, container, false)
+        val vkbtn = root.findViewById<Button>(R.id.vkbtn)
+        vkbtn.setOnClickListener{
+            VK.login(requireActivity(), arrayListOf(VKScope.WALL, VKScope.PHOTOS))
         }
-
-
-
 
 
         return root
     }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val callback = object: VKAuthCallback {
+            override fun onLogin(token: VKAccessToken) {
+                // User passed authorization
+            }
+
+            override fun onLoginFailed(errorCode: Int) {
+                // User didn't pass authorization
+            }
+        }
+        if (data == null || !VK.onActivityResult(requestCode, resultCode, data, callback)) {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+
 }
+
+
+
+
--- app/src/main/java/com/example/lunin_ia_191_352/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/lunin_ia_191_352/ui/notifications/NotificationsFragment.kt
@@ -1,41 +1,25 @@
 package com.example.lunin_ia_191_352.ui.notifications
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.Toast
-import androidx.camera.core.CameraSelector
-import androidx.camera.core.ImageCapture
-import androidx.camera.core.ImageCaptureException
-import androidx.camera.core.Preview
-import androidx.camera.lifecycle.ProcessCameraProvider
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import com.example.lunin_ia_191_352.R
-import kotlinx.android.synthetic.main.fragment_notifications.*
-import java.io.File
-import java.text.SimpleDateFormat
-import java.util.*
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
 import androidx.lifecycle.ViewModelProvider
-
-typealias LumaListener = (luma: Double) -> Unit
+import com.example.lunin_ia_191_352.R
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
 
 
 class NotificationsFragment : Fragment() {
 
     private lateinit var notificationsViewModel: NotificationsViewModel
-    private lateinit var outputDirectory: File
-    private lateinit var cameraExecutor: ExecutorService
-    private var imageCapture: ImageCapture? = null
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -45,146 +29,36 @@ class NotificationsFragment : Fragment() {
         notificationsViewModel =
             ViewModelProvider(this).get(NotificationsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_notifications, container, false)
+        val btn = root.findViewById<Button>(R.id.button)
+        var text_html = root.findViewById<TextView>(R.id.editTextTextMultiLine)
+        var curs_backsa = root.findViewById<TextView>(R.id.textView2)
 
+        btn.setOnClickListener{
+            GlobalScope.async {
+                val doc: Document = Jsoup.connect("https://www.cbr.ru/").get()
+                val parse_test = doc.text()
+                val mainHeaderElements: Elements = doc.select("#content > div > div > div > div.home-main > div.home-main_aside > div > div.main-indicator_rates > div > div:nth-child(2) > div:nth-child(2)")
+                val test : String = mainHeaderElements.toString()
+                val oldValue = "<div class=\"col-md-2 col-xs-9 _right mono-num\">"
+                val newValue = ""
+                val test2 = test.replace(oldValue,newValue,ignoreCase=true)
+                val oldValue2 = "</div>"
+                val final = test2.replace(oldValue2,newValue,ignoreCase=true)
+                while (curs_backsa.toString().isNotEmpty() and text_html.toString().isNotEmpty()){
+                    curs_backsa.text = final
+                    text_html.text = parse_test
+                }
 
-
-
-
-        val photoBtn: Button = root.findViewById(R.id.camera_capture_button)
-
-        // Request camera permissions
-        if (allPermissionsGranted()) {
-            startCamera()
-        } else {
-            ActivityCompat.requestPermissions(
-                requireActivity(), REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
-            )
+            }
         }
 
-            // Set up the listener for take photo button
-            photoBtn.setOnClickListener { takePhoto() }
 
-            outputDirectory = getOutputDirectory()
 
-            cameraExecutor = Executors.newSingleThreadExecutor()
 
 
         return root
     }
 
 
-    private fun takePhoto() {
-        // Get a stable reference of the modifiable image capture use case
-        val imageCapture = imageCapture ?: return
-
-        // Create time-stamped output file to hold the image
-        val photoFile = File(
-            outputDirectory,
-            SimpleDateFormat(
-                FILENAME_FORMAT, Locale.US
-            ).format(System.currentTimeMillis()) + ".jpg"
-        )
-
-        // Create output options object which contains file + metadata
-        val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile).build()
-
-        // Set up image capture listener, which is triggered after photo has
-        // been taken
-        imageCapture.takePicture(
-            outputOptions,
-            ContextCompat.getMainExecutor(requireContext()),
-            object : ImageCapture.OnImageSavedCallback {
-                override fun onError(exc: ImageCaptureException) {
-                    Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
-                }
-
-                override fun onImageSaved(output: ImageCapture.OutputFileResults) {
-                    val savedUri = Uri.fromFile(photoFile)
-                    val msg = "Photo capture succeeded: $savedUri"
-                    Toast.makeText(requireContext(), msg, Toast.LENGTH_SHORT).show()
-                    Log.d(TAG, msg)
-                }
-            })
-
-    }
-
-    private fun startCamera() {
-        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
-
-        cameraProviderFuture.addListener(Runnable {
-            // Used to bind the lifecycle of cameras to the lifecycle owner
-            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
-
-            // Preview
-            val preview = Preview.Builder()
-                .build()
-                .also {
-                    it.setSurfaceProvider(viewFinder.createSurfaceProvider())
-                }
-
-            imageCapture = ImageCapture.Builder()
-                .build()
-
-            // Select back camera as a default
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-            try {
-                // Unbind use cases before rebinding
-                cameraProvider.unbindAll()
-
-                // Bind use cases to camera
-                cameraProvider.bindToLifecycle(
-                    this, cameraSelector, preview, imageCapture
-                )
-
-            } catch (exc: Exception) {
-                Log.e(TAG, "Use case binding failed", exc)
-            }
-
-        }, ContextCompat.getMainExecutor(requireContext()))
-    }
-
-    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(
-            requireContext(), it
-        ) == PackageManager.PERMISSION_GRANTED
-    }
-
-    private fun getOutputDirectory(): File {
-        val mediaDir = requireActivity().externalMediaDirs.firstOrNull()?.let {
-            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
-        }
-        return if (mediaDir != null && mediaDir.exists())
-            mediaDir else requireActivity().filesDir
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        cameraExecutor.shutdown()
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int, permissions: Array<String>, grantResults:
-        IntArray
-    ) {
-        if (requestCode == REQUEST_CODE_PERMISSIONS) {
-            if (allPermissionsGranted()) {
-                startCamera()
-            } else {
-                Toast.makeText(
-                    requireContext(),
-                    "Permissions not granted by the user.",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-        }
-    }
-
-    companion object {
-        private const val TAG = "CameraXBasic"
-        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
-        private const val REQUEST_CODE_PERMISSIONS = 10
-        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
-    }
 
 }
