3f623c83881f74132b41189d0b99aa1ab379945b
Ivan <twist9743@mail.ru>
2021-04-15 10:49:26 +0300

    lab4

--- app/src/main/java/com/example/lunin_ia_191_352/MainActivity.kt
+++ app/src/main/java/com/example/lunin_ia_191_352/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.lunin_ia_191_352
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.widget.MediaController
@@ -8,13 +9,13 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.vk.api.sdk.VK
+import com.vk.api.sdk.auth.VKAccessToken
+import com.vk.api.sdk.auth.VKAuthCallback
+import kotlinx.android.synthetic.main.fragment_lab4.*
 
 
 class MainActivity : AppCompatActivity() {
-
-
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,10 +28,22 @@ class MainActivity : AppCompatActivity() {
 
         
         navView.setupWithNavController(navController)
-
-
-
-
-
     }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        val callback = object : VKAuthCallback {
+            override fun onLogin(token: VKAccessToken) {
+                textView3.text = token.accessToken
+            }
+
+            override fun onLoginFailed(errorCode: Int) {
+                // User didn't pass authorization
+            }
+        }
+        if (data == null || !VK.onActivityResult(requestCode, resultCode, data, callback)) {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/lunin_ia_191_352/ui/Lab3/Lab3.kt
+++ app/src/main/java/com/example/lunin_ia_191_352/ui/Lab3/Lab3.kt
@@ -1,18 +1,22 @@
 package com.example.lunin_ia_191_352.ui.lab3
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.lunin_ia_191_352.MainActivity
 import com.example.lunin_ia_191_352.R
 import com.vk.api.sdk.VK
 import com.vk.api.sdk.auth.VKAccessToken
 import com.vk.api.sdk.auth.VKAuthCallback
 import com.vk.api.sdk.auth.VKScope
+import com.vk.api.sdk.ui.VKWebViewAuthActivity
 
 
 class Lab3Fragment : Fragment() {
@@ -27,30 +31,29 @@ class Lab3Fragment : Fragment() {
         lab3ViewModel =
             ViewModelProvider(this).get(Lab3ViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_lab4, container, false)
-        val vkbtn = root.findViewById<Button>(R.id.vkbtn)
-        vkbtn.setOnClickListener{
+        val vkBtn = root.findViewById<Button>(R.id.vkbtn)
+        val vkToken = root.findViewById<TextView>(R.id.textView3)
+        vkBtn.setOnClickListener {
             VK.login(requireActivity(), arrayListOf(VKScope.WALL, VKScope.PHOTOS))
+            if (VK.isLoggedIn()) {
+                val token = VKAccessToken.restore(
+                    activity?.getSharedPreferences(
+                        "com.vkontakte.android_pref_name",
+                        Context.MODE_PRIVATE))
+                vkToken.text = token?.accessToken
+            }
         }
-
-
         return root
     }
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        val callback = object: VKAuthCallback {
-            override fun onLogin(token: VKAccessToken) {
-                // User passed authorization
-            }
 
-            override fun onLoginFailed(errorCode: Int) {
-                // User didn't pass authorization
-            }
-        }
-        if (data == null || !VK.onActivityResult(requestCode, resultCode, data, callback)) {
-            super.onActivityResult(requestCode, resultCode, data)
+
+    companion object {
+        fun startFrom(context: Context) {
+            val intent = Intent(context, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+            context.startActivity(intent)
         }
     }
-
-
 }
 
 
