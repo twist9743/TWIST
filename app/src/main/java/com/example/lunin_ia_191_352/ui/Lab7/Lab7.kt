11a6b53397becf12874678788a2158aad9332fb6
Ivan <twist9743@mail.ru>
2021-06-10 00:42:05 +0300

    lab7_fix_chat

--- app/src/main/java/com/example/lunin_ia_191_352/ui/Lab7/Lab7.kt
+++ app/src/main/java/com/example/lunin_ia_191_352/ui/Lab7/Lab7.kt
@@ -11,8 +11,10 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.lunin_ia_191_352.R
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import okhttp3.internal.notify
 import org.java_websocket.client.WebSocketClient
 import org.java_websocket.handshake.ServerHandshake
 import org.json.JSONObject
@@ -63,6 +65,7 @@ class Lab7Fragment : Fragment() {
             messagesAll.add(Message("user", message,date))
             wss_client.send(message)
             user_request.text.clear()
+            (messagesRecyclerView.adapter as Messages).notifyDataSetChanged()
 
 
         }
@@ -79,11 +82,12 @@ class Lab7Fragment : Fragment() {
             override fun onMessage(message: String?) {
                 if (message != null) {
                     val jsonmessage = JSONObject(message)
-                    GlobalScope.launch {
+                    GlobalScope.launch(Dispatchers.Main) {
                         Log.d("Socket",message)
                         Log.d("body", jsonmessage.get("body") as String)
                         val date = SimpleDateFormat("HH:mm", Locale.getDefault()).format(Date())
                         messagesAll.add(Message("Bot", jsonmessage.get("body") as String,date))
+                        (messagesRecyclerView.adapter as Messages).notifyDataSetChanged()
                     }
                 }
             }
@@ -102,64 +106,59 @@ class Lab7Fragment : Fragment() {
     data class Message(val who:String, val text:String, val date: String)
         
     
-    class Messages(private val messagesList: MutableList<Message>) :
-        RecyclerView.Adapter<Messages.MsgViewHolder>() {
-        inner class MsgViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-            var bot_text: TextView = itemView.findViewById(R.id.bot_msg_model)
-            var user_text: TextView = itemView.findViewById(R.id.user_msg_model)
-            var bot_layout :LinearLayout = itemView.findViewById(R.id.bot_layout)
-            var user_layout :LinearLayout = itemView.findViewById(R.id.user_layout)
-            var bot_spacer : Space = itemView.findViewById(R.id.bot_spacer)
-            var user_spacer : Space = itemView.findViewById(R.id.user_spacer)
-            var bot_date : TextView = itemView.findViewById(R.id.bot_date)
-            var user_date : TextView = itemView.findViewById(R.id.user_date)
-        }
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MsgViewHolder {
-            val itemView =
-                LayoutInflater.from(parent.context).inflate(R.layout.user_message, parent, false)
-            return MsgViewHolder(itemView)
-        }
 
-        override fun onBindViewHolder(holder: MsgViewHolder, position: Int) {
-            val item = messagesList[position]
-            holder.apply {
-                if (item.who=="user"){
-                    user_text.text=item.text
-                    bot_layout.visibility = View.GONE
-                    user_layout.visibility = View.VISIBLE
-                    user_spacer.visibility = View.VISIBLE
-                    bot_spacer.visibility=View.GONE
-                    bot_date.visibility = View.GONE
-                    user_date.visibility = View.VISIBLE
-                    user_date.text=item.date
-                }
-                else{
-                    bot_text.text=item.text
-                    user_layout.visibility = View.GONE
-                    bot_layout.visibility = View.VISIBLE
-                    bot_spacer.visibility = View.VISIBLE
-                    user_spacer.visibility=View.GONE
-                    user_date.visibility = View.GONE
-                    bot_date.visibility = View.VISIBLE
-                    bot_date.text=item.date
-                }
-            }
 
-        }
+}
+class Messages(private val messagesList: MutableList<Lab7Fragment.Message>) :
+    RecyclerView.Adapter<Messages.MsgViewHolder>() {
+    inner class MsgViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-        override fun getItemCount() = messagesList.size
-
-        fun updateData(newMsgs: MutableList<Message>){
-            this.apply {
-                messagesList.clear()
-                messagesList.addAll(newMsgs)
-            }
-            notifyDataSetChanged()
-        }
+        var bot_text: TextView = itemView.findViewById(R.id.bot_msg_model)
+        var user_text: TextView = itemView.findViewById(R.id.user_msg_model)
+        var bot_layout :LinearLayout = itemView.findViewById(R.id.bot_layout)
+        var user_layout :LinearLayout = itemView.findViewById(R.id.user_layout)
+        var bot_spacer : Space = itemView.findViewById(R.id.bot_spacer)
+        var user_spacer : Space = itemView.findViewById(R.id.user_spacer)
+        var bot_date : TextView = itemView.findViewById(R.id.bot_date)
+        var user_date : TextView = itemView.findViewById(R.id.user_date)
     }
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MsgViewHolder {
+        val itemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.user_message, parent, false)
+        return MsgViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MsgViewHolder, position: Int) {
+        val item = messagesList[position]
+        holder.apply {
+            if (item.who=="user"){
+                user_text.text=item.text
+                bot_layout.visibility = View.GONE
+                user_layout.visibility = View.VISIBLE
+                user_spacer.visibility = View.VISIBLE
+                bot_spacer.visibility=View.GONE
+                bot_date.visibility = View.GONE
+                user_date.visibility = View.VISIBLE
+                user_date.text=item.date
+            }
+            else{
+                bot_text.text=item.text
+                user_layout.visibility = View.GONE
+                bot_layout.visibility = View.VISIBLE
+                bot_spacer.visibility = View.VISIBLE
+                user_spacer.visibility=View.GONE
+                user_date.visibility = View.GONE
+                bot_date.visibility = View.VISIBLE
+                bot_date.text=item.date
+            }
+        }
+
+    }
+
+    override fun getItemCount() = messagesList.size
 
 
 }
